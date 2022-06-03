2b541cbe82f313e0b6b5fe6f0306cbc6fcb27cf9
rofequlislam <rofequlislamnayem@gmail.com>
2022-06-03 15:41:07 +0600

    first commit

---
+++ 01-laravel-vue-survey/vue/src/router/index.js
@@ -0,0 +1,54 @@
+import {createRouter, createWebHistory} from "vue-router";
+import store from "../store";
+
+const routes = [
+    {
+        path: '/',
+        redirect: '/dashboard',
+        component: import("../components/DefaultLayout.vue"),
+        meta: {requiresAuth: true},
+        children: [
+            {
+                path: '/dashboard',
+                name: 'Dashboard',
+                component: () => import("../views/Dashboard.vue")
+            },
+            {
+                path: '/surveys',
+                name: 'Surveys',
+                component: () => import("../views/Surveys.vue")
+            }
+        ]
+    },
+    {
+        path: '/auth',
+        redirect: '/login',
+        name: 'Auth',
+        component: import("../components/AuthLayout.vue"),
+        children: [
+            {
+                path: '/login',
+                name: 'Login',
+                component: () => import("../views/Login.vue")
+            },
+            {
+                path: '/register',
+                name: 'Register',
+                component: () => import("../views/Register.vue")
+            },
+        ]
+    }
+];
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes
+})
+
+router.beforeEach((to, from, next) => {
+    if (to.meta.requiresAuth && !store.state.user.token) next({name: 'Login'})
+    else if (store.state.user.token && (to.name === 'Login' || to.name === 'Register')) next({name: 'Dashboard'})
+    else next()
+})
+
+export default router;
