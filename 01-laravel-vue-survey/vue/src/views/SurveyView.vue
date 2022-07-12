e3b6bded2108782e398868a5a2e199cf344a440b
rofequlislam <rofequlislamnayem@gmail.com>
2022-07-12 18:42:37 +0600

    ok

--- 01-laravel-vue-survey/vue/src/views/SurveyView.vue
+++ 01-laravel-vue-survey/vue/src/views/SurveyView.vue
@@ -18,23 +18,39 @@
                         </label>
                         <div class="mt-1 flex items-center">
                             <img v-if="model.image" :src="model.image" :alt="model.title"
-                                 class="w-64 h-48 object-cover">
+                                class="w-64 h-48 object-cover">
                             <span v-else
-                                  class="flex items-center justify-center h-12 w-12 rounded-full overflow-hidden bg-gray-100">
+                                class="flex items-center justify-center h-12 w-12 rounded-full overflow-hidden bg-gray-100">
                                 <svg xmlns="http://www.w3.org/2000/svg" class="h-[80%] w-[80%] text-gray-300"
-                                     fill="none"
-                                     viewBox="0 0 24 24"
-                                     stroke="currentColor" stroke-width="2">
-  <path stroke-linecap="round" stroke-linejoin="round"
-        d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z"/>
-</svg>
+                                    fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                                    <path stroke-linecap="round" stroke-linejoin="round"
+                                        d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z" />
+                                </svg>
                             </span>
                             <button type="button"
-                                    class="relative overflow-hidden ml-5 bg-white py-2 px-3 border border-gray-300 rounded-md shadow-sm text-sm leading-4 front-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                                class="relative overflow-hidden ml-5 bg-white py-2 px-3 border border-gray-300 rounded-md shadow-sm text-sm leading-4 front-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                                <input type="file"
+                                    class="absolute left-0 top-0 right-0 bottom-0 opacity-0 cursor-pointer">
                                 Change
                             </button>
                         </div>
                     </div>
+                    <!--End Image-->
+                    <!-- Title -->
+                    <div>
+                        <label for="title" class="block text-sm font-medium text-gray-700">Title</label>
+                        <input type="text" name="title" id="title" v-model="model.title" autocomplete="survey_title"
+                            class="mt-1 focus:ring-indigo-500 block w-full shadow-sm sm:text-sm border-gray-500 rounded-md">
+                    </div>
+                    <!-- End title -->
+                    <!-- Description -->
+                    <div>
+                        <label for="about" class="block text-sm font-medium text-gray-700">Description</label>
+                        <div class="mt-1">
+                            <textarea name="description" id="description"></textarea>
+                        </div>
+                    </div>
+                    <!-- End Description -->
                 </div>
             </div>
         </form>
