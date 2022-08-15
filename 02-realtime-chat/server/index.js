635c6d0386506fb98820c8050650f45a043322f4
rofequlislam <rofequlislamnayem@gmail.com>
2022-08-15 14:26:39 +0600

    Realtime chat Post Api create

--- 02-realtime-chat/server/index.js
+++ 02-realtime-chat/server/index.js
@@ -1,27 +1,11 @@
-require("dotenv").config();
-require('./db')
-const express = require("express");
-const cors = require("cors");
+const express = require('express')
+const app = express()
+const port = 3000
 
-const PORT = process.env.PORT || 3000;
-const app = express();
-// Init body-parser options (inbuilt with express)
-app.use(express.json());
-app.use(express.static('storage'))
-app.use(cors());
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
 
-// Import API routes
-const router = require('./routes/api.js')
-// Use API Routes
-app.use('/api', router)
-
-app.use((error, req, res, next) => {
-    console.log(error);
-    const status = error.statusCode || 500;
-    const message = error.message;
-    const data = error.data;
-    res.status(status).json({message, data});
-});
-
-
-app.listen(PORT, () => console.log(`app started on port: ${PORT}`));
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
