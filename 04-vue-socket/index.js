c8ab0f6979a8556dd0fdf09fdf2f46b96e9769b6
rofequlislam <rofequlislamnayem@gmail.com>
2022-10-28 14:48:33 +0600

    New Socket project startup

--- 04-vue-socket/index.js
+++ 04-vue-socket/index.js
@@ -7,7 +7,9 @@ const app = express();
 app.use('/',express.static('public'))
 
 // Frontend view html page serve
-
+app.get('/', function (req, res) {
+    res.sendFile("index.html", {root: path.join(__dirname, 'public')})
+});
 
 app.listen(PORT, () => console.log(`app started on port: ${PORT}`));
 
