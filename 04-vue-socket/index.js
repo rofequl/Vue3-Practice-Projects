cc815399fb9d2f4b2df6e8b0d6015496ed535f01
rofequlislam <rofequlislamnayem@gmail.com>
2022-10-28 14:45:50 +0600

    New Socket project startup

--- 04-vue-socket/index.js
+++ 04-vue-socket/index.js
@@ -4,12 +4,10 @@ const path = require('path');
 const PORT = process.env.PORT || 3000;
 const app = express();
 // Init body-parser options (inbuilt with express)
-app.use(express.static('public'))
+app.use('/',express.static('public'))
 
 // Frontend view html page serve
-app.get('/', function (req, res) {
-    res.sendFile("index.html", {root: path.join(__dirname, 'public')})
-});
+
 
 app.listen(PORT, () => console.log(`app started on port: ${PORT}`));
 
