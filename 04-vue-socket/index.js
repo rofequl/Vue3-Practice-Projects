3f1915f6495bbfa67524c84b757f274922f53ca6
rofequlislam <rofequlislamnayem@gmail.com>
2022-10-28 08:58:25 +0600

    New Socket project startup

--- 04-vue-socket/index.js
+++ 04-vue-socket/index.js
@@ -1,14 +1,15 @@
 const express = require("express");
-const app = express();
-
-const cors = require('cors')
+const cors = require("cors");
 
 const path = __dirname + '/views/dist/';
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
+const app = express();
+// Init body-parser options (inbuilt with express)
 app.use(express.json());
 app.use(express.static(path))
 app.use(cors());
 
+// Frontend view html page serve
 app.get('/', function (req, res) {
     res.sendFile(path + "index.html")
 });
